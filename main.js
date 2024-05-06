fb0b3a9767092f09c20359c4ac41df6efec6cf27
Miller Arias <millerjavierarias@gmail.com>
2024-05-06 14:21:16 -0500

    Feat:lightbox and refactor:main.js

--- main.js
+++ main.js
@@ -5,33 +5,42 @@ import { v4 as uuidv4 } from "uuid";
 const cartItems = [];
 const app = document.querySelector("#app");
 // header
-const header = document.createElement("header");
-const menuContainer = document.createElement("div");
-menuContainer.classList.add("menu__container");
-const menuButton = document.createElement("button");
-menuButton.classList.add("menu__button");
-menuButton.id = "menu";
-menuButton.innerHTML = `
+
+const createMenuContainer = () => {
+	const menuContainer = document.createElement("div");
+	menuContainer.classList.add("menu__container");
+	const menuButton = document.createElement("button");
+	menuButton.classList.add("menu__button");
+	menuButton.id = "menu";
+	menuButton.innerHTML = `
   <img src="./assets/icon-menu.svg" alt="menu"/>
 `;
-const title = document.createElement("h1");
-title.textContent = "sneakers";
-const cartContainer = document.createElement("div");
-cartContainer.classList.add("cart__container");
-const cartButton = document.createElement("button");
-cartButton.classList.add("cart__button");
-cartButton.innerHTML = `
+	const title = document.createElement("h1");
+	title.textContent = "sneakers";
+	menuContainer.append(menuButton, title);
+	return menuContainer;
+};
+const createCartContainer = () => {
+	const cartContainer = document.createElement("div");
+	cartContainer.classList.add("cart__container");
+	const cartButton = document.createElement("button");
+	cartButton.classList.add("cart__button");
+	cartButton.innerHTML = `
   <span class="hidden__number"></span>
   <img src="./assets/icon-cart.svg" alt="cart"/>
 `;
-const profile = document.createElement("button");
-profile.innerHTML = `
+	const profile = document.createElement("button");
+	profile.innerHTML = `
   <img src="./assets/image-avatar.png" alt="profile-avatar"/>
 `;
-const cartInfo = document.createElement("div");
-cartInfo.classList.add("cart__info");
-cartInfo.classList.add("hidden__cart");
-cartInfo.innerHTML = `
+	cartContainer.append(cartButton, profile);
+	return cartContainer;
+};
+const createCartInfo = () => {
+	const cartInfo = document.createElement("div");
+	cartInfo.classList.add("cart__info");
+	cartInfo.classList.add("hidden__cart");
+	cartInfo.innerHTML = `
   <div>
     <p>Cart</p>
   </div>
@@ -39,158 +48,201 @@ cartInfo.innerHTML = `
     <p>Your cart is empty</p>
   </div>  
 `;
+	return cartInfo;
+};
+
 // menu mobile
-const menu = document.createElement("aside");
-menu.classList.add("hidden");
-const closeButton = document.createElement("button");
-closeButton.id = "close";
-closeButton.innerHTML = `
+const createMenu = () => {
+	const menu = document.createElement("aside");
+	menu.classList.add("hidden");
+	const closeButton = document.createElement("button");
+	closeButton.id = "close";
+	closeButton.innerHTML = `
   <img src="./assets/icon-close.svg" alt="close menu"/>
 `;
-const categories = document.createElement("ul");
-categories.innerHTML = `
+	const categories = document.createElement("ul");
+	categories.innerHTML = `
   <li>Collections</li>
   <li>Men</li>
   <li>Women</li>
   <li>About</li>
   <li>Contact</li>
 `;
-menu.append(closeButton, categories);
+	menu.append(closeButton, categories);
+	return menu;
+};
+const menu = createMenu();
 
 // main article
-const mainContainer = document.createElement("main");
-const article = document.createElement("article");
-const carrouselContainer = document.createElement("div");
-carrouselContainer.classList.add("carrousel__container");
-const carrousel = document.createElement("div");
-const principalImage = document.createElement("img");
-principalImage.classList.add("principal__image");
-principalImage.src = "./assets/image-product-1.jpg";
-carrousel.classList.add("carrousel__images");
-const carrouselImg1 = document.createElement("img");
-const carrouselImg2 = document.createElement("img");
-const carrouselImg3 = document.createElement("img");
-const carrouselImg4 = document.createElement("img");
-carrouselImg1.src = "./assets/image-product-1.jpg";
-carrouselImg2.src = "./assets/image-product-2.jpg";
-carrouselImg3.src = "./assets/image-product-3.jpg";
-carrouselImg4.src = "./assets/image-product-4.jpg";
-if (window.innerWidth < 1280) {
-	carrouselImg2.classList.add("hidden__image");
-	carrouselImg3.classList.add("hidden__image");
-	carrouselImg4.classList.add("hidden__image");
-}
+const createMainContainer = () => {
+	const mainContainer = document.createElement("main");
+	const article = createArticle();
+	mainContainer.append(article);
+	return mainContainer;
+};
+const createArticle = () => {
+	const article = document.createElement("article");
+	const carrouselContainer = createCarrousel();
+	const infoContainer = createInfoContainer();
+	article.append(carrouselContainer, infoContainer);
+	return article;
+};
+const createCarrousel = () => {
+	const carrouselContainer = document.createElement("div");
+	carrouselContainer.classList.add("carrousel__container");
+	const carrousel = document.createElement("div");
+	const principalImage = document.createElement("img");
+	principalImage.classList.add("principal__image");
+	principalImage.src = "./assets/image-product-1.jpg";
+	carrousel.classList.add("carrousel__images");
+	const carrouselImg1 = document.createElement("img");
+	const carrouselImg2 = document.createElement("img");
+	const carrouselImg3 = document.createElement("img");
+	const carrouselImg4 = document.createElement("img");
+	carrouselImg1.src = "./assets/image-product-1.jpg";
+	carrouselImg2.src = "./assets/image-product-2.jpg";
+	carrouselImg3.src = "./assets/image-product-3.jpg";
+	carrouselImg4.src = "./assets/image-product-4.jpg";
+	if (window.innerWidth < 1280) {
+		carrouselImg2.classList.add("hidden__image");
+		carrouselImg3.classList.add("hidden__image");
+		carrouselImg4.classList.add("hidden__image");
+	}
 
-const leftButtonCarrousel = document.createElement("button");
-leftButtonCarrousel.id = "previous";
-leftButtonCarrousel.innerHTML = `
+	const leftButtonCarrousel = document.createElement("button");
+	leftButtonCarrousel.id = "previous";
+	leftButtonCarrousel.innerHTML = `
   <img src="./assets/icon-previous.svg" alt="previous button"/>
 `;
-leftButtonCarrousel.classList.add("previous-button__carrousel");
-const rightButtonCarrousel = document.createElement("button");
-rightButtonCarrousel.id = "next";
-rightButtonCarrousel.innerHTML = `
+	leftButtonCarrousel.classList.add("previous-button__carrousel");
+	const rightButtonCarrousel = document.createElement("button");
+	rightButtonCarrousel.id = "next";
+	rightButtonCarrousel.innerHTML = `
   <img src="./assets/icon-next.svg" alt="next button"/>
 `;
-rightButtonCarrousel.classList.add("next-button__carrousel");
-const infoContainer = document.createElement("div");
-infoContainer.classList.add("info__container");
-const company = document.createElement("span");
-company.textContent = "SNEAKER COMPANY";
-company.classList.add("company__info");
-const name = document.createElement("h2");
-name.textContent = "Fall Limited Edition Sneakers";
-name.classList.add("name__info");
-const description = document.createElement("p");
-description.textContent =
-	"These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.";
-description.classList.add("description__info");
-const pricesContainer = document.createElement("div");
-pricesContainer.classList.add("prices__container");
-const discountContainer = document.createElement("div");
-discountContainer.classList.add("discount__container");
-const discountPrice = document.createElement("span");
-discountPrice.textContent = "125.00";
-discountPrice.classList.add("discount__price");
-const discount = document.createElement("span");
-discount.textContent = "50%";
-discount.classList.add("discount__percentage");
-const normalPrice = document.createElement("span");
-normalPrice.textContent = "250.00";
-normalPrice.classList.add("normal__price");
-const buyContainer = document.createElement("div");
-buyContainer.classList.add("buy__container");
-const quantityContainer = document.createElement("div");
-quantityContainer.classList.add("quantity__container");
-const leftButtonQuantity = document.createElement("button");
-leftButtonQuantity.id = "minus";
-leftButtonQuantity.disabled = "true";
-leftButtonQuantity.innerHTML = `
+	rightButtonCarrousel.classList.add("next-button__carrousel");
+	if (window.innerWidth > 1280) {
+		carrousel.append(
+			principalImage,
+			carrouselImg1,
+			carrouselImg2,
+			carrouselImg3,
+			carrouselImg4
+		);
+	} else {
+		carrousel.append(
+			carrouselImg1,
+			carrouselImg2,
+			carrouselImg3,
+			carrouselImg4
+		);
+	}
+	carrouselContainer.append(
+		leftButtonCarrousel,
+		rightButtonCarrousel,
+		carrousel
+	);
+	return carrouselContainer;
+};
+
+const createInfoContainer = () => {
+	const infoContainer = document.createElement("div");
+	infoContainer.classList.add("info__container");
+	const company = document.createElement("span");
+	company.textContent = "SNEAKER COMPANY";
+	company.classList.add("company__info");
+	const name = document.createElement("h2");
+	name.textContent = "Fall Limited Edition Sneakers";
+	name.classList.add("name__info");
+	const description = document.createElement("p");
+	description.textContent =
+		"These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.";
+	description.classList.add("description__info");
+	const pricesContainer = document.createElement("div");
+	pricesContainer.classList.add("prices__container");
+	const discountContainer = document.createElement("div");
+	discountContainer.classList.add("discount__container");
+	const discountPrice = document.createElement("span");
+	discountPrice.textContent = "125.00";
+	discountPrice.classList.add("discount__price");
+	const discount = document.createElement("span");
+	discount.textContent = "50%";
+	discount.classList.add("discount__percentage");
+	const normalPrice = document.createElement("span");
+	normalPrice.textContent = "250.00";
+	normalPrice.classList.add("normal__price");
+	const buyContainer = document.createElement("div");
+	buyContainer.classList.add("buy__container");
+	const quantityContainer = document.createElement("div");
+	quantityContainer.classList.add("quantity__container");
+	const leftButtonQuantity = document.createElement("button");
+	leftButtonQuantity.id = "minus";
+	leftButtonQuantity.disabled = "true";
+	leftButtonQuantity.innerHTML = `
   <img src="./assets/icon-minus.svg" alt="minus button"/>
 `;
-const amount = document.createElement("span");
-amount.textContent = 0;
-const rightButtonQuantity = document.createElement("button");
-rightButtonQuantity.id = "plus";
-rightButtonQuantity.innerHTML = `
+	const amount = document.createElement("span");
+	amount.textContent = 0;
+	const rightButtonQuantity = document.createElement("button");
+	rightButtonQuantity.id = "plus";
+	rightButtonQuantity.innerHTML = `
   <img src="./assets/icon-plus.svg" alt="more button"/>
 `;
-const addCartButton = document.createElement("button");
-addCartButton.id = "buy";
-addCartButton.innerHTML = `
+	const addCartButton = document.createElement("button");
+	addCartButton.id = "buy";
+	addCartButton.innerHTML = `
   <img src="./assets/icon-cart.svg" alt="button cart"/>
   <p>Add to cart</p>
 `;
-addCartButton.classList.add("addCart__button");
-addCartButton.disabled = true;
-if (window.innerWidth > 1280) {
-	carrousel.append(
-		principalImage,
-		carrouselImg1,
-		carrouselImg2,
-		carrouselImg3,
-		carrouselImg4
-	);
-} else {
-	carrousel.append(carrouselImg1, carrouselImg2, carrouselImg3, carrouselImg4);
-}
-buyContainer.append(quantityContainer, addCartButton);
-carrouselContainer.append(leftButtonCarrousel, rightButtonCarrousel, carrousel);
-discountContainer.append(discountPrice, discount);
-pricesContainer.append(discountContainer, normalPrice);
-quantityContainer.append(leftButtonQuantity, amount, rightButtonQuantity);
+	addCartButton.classList.add("addCart__button");
+	addCartButton.disabled = true;
+	buyContainer.append(quantityContainer, addCartButton);
+	discountContainer.append(discountPrice, discount);
+	pricesContainer.append(discountContainer, normalPrice);
+	quantityContainer.append(leftButtonQuantity, amount, rightButtonQuantity);
+	if (window.innerWidth < 1280) {
+		infoContainer.append(
+			company,
+			name,
+			description,
+			pricesContainer,
+			quantityContainer,
+			addCartButton
+		);
+	} else {
+		infoContainer.append(
+			company,
+			name,
+			description,
+			pricesContainer,
+			buyContainer
+		);
+	}
+	return infoContainer;
+};
 
-if (window.innerWidth < 1280) {
-	infoContainer.append(
-		company,
-		name,
-		description,
-		pricesContainer,
-		quantityContainer,
-		addCartButton
-	);
-} else {
-	infoContainer.append(
-		company,
-		name,
-		description,
-		pricesContainer,
-		buyContainer
-	);
-}
+const mainContainer = createMainContainer();
 
-article.append(carrouselContainer, infoContainer);
-mainContainer.append(article);
-
-menuContainer.append(menuButton, title);
-cartContainer.append(cartButton, profile);
-
+const createHeader = () => {
+	const header = document.createElement("header");
+	const menuContainer = createMenuContainer();
+	const cartContainer = createCartContainer();
+	const cartInfo = createCartInfo();
+	const menu = createMenu();
+	if (window.innerWidth < 767) {
+		header.append(menuContainer, cartContainer, cartInfo);
+	} else {
+		menu.classList.remove("hidden");
+		header.append(menuContainer, menu, cartContainer, cartInfo);
+	}
+	return header;
+};
+const header = createHeader();
 if (window.innerWidth < 767) {
-	header.append(menuContainer, cartContainer, cartInfo);
 	app.append(header, menu, mainContainer);
 } else {
 	menu.classList.remove("hidden");
-	header.append(menuContainer, menu, cartContainer, cartInfo);
+
 	app.append(header, mainContainer);
 }
 
@@ -199,7 +251,7 @@ const close = document.querySelector("#close");
 const hamburguer = document.querySelector("#menu");
 
 const cartBtn = document.querySelector(".cart__button");
-
+const cartInfo = document.querySelector(".cart__info");
 const carrouselImages = Array.from(
 	document.querySelectorAll(".carrousel__images img")
 );
@@ -208,18 +260,24 @@ const next = document.querySelector("#next");
 
 const minus = document.querySelector("#minus");
 const plus = document.querySelector("#plus");
-
+const amount = document.querySelector(".quantity__container span");
 const buy = document.querySelector("#buy");
-
+const images = Array.from(document.querySelectorAll(".carrousel__images img"));
+const carrouselImg1 = images[0];
+const discountPrice = document.querySelector(".discount__price");
+const name = document.querySelector(".name__info");
+const principalImage = document.querySelector(".principal__image");
 let carrouselCount = 0;
 let amountCount = 0;
-
-close.addEventListener("click", () => {
-	menu.classList.add("hidden");
-});
-hamburguer.addEventListener("click", () => {
-	menu.classList.remove("hidden");
-});
+console.log(principalImage);
+if (window.innerWidth < 768) {
+	close.addEventListener("click", () => {
+		menu.classList.add("hidden");
+	});
+	hamburguer.addEventListener("click", () => {
+		menu.classList.remove("hidden");
+	});
+}
 
 cartBtn.addEventListener("click", () => {
 	cartInfo.classList.toggle("hidden__cart");
@@ -359,7 +417,6 @@ const updateCartCounter = () => {
 	}
 };
 
-const images = Array.from(document.querySelectorAll(".carrousel__images img"));
 images[1].classList.add("selected");
 
 images.forEach((image) =>
@@ -373,3 +430,49 @@ images.forEach((image) =>
 		e.target.classList.add("selected");
 	})
 );
+let isModalOpen = false;
+const openModal = () => {
+	if (isModalOpen) return;
+	isModalOpen = true;
+	const modal = document.createElement("div");
+	modal.classList.add("modal");
+	const closeButton = document.createElement("button");
+	closeButton.id = "close";
+	closeButton.innerHTML = `
+  <img src="./assets/icon-close.svg" alt="close menu"/>
+`;
+	const carouselModal = createCarrousel();
+	modal.append(closeButton, carouselModal);
+	app.appendChild(modal);
+	const carouselModalImages = Array.from(
+		document.querySelectorAll(".modal .carrousel__images img")
+	);
+	const principalImage = document.querySelector(
+		".modal .carrousel__images .principal__image"
+	);
+	const previous = document.querySelector(".modal #previous");
+	const next = document.querySelector(".modal #next");
+	next.addEventListener("click", () => {
+		carrouselCount++;
+		if (carrouselCount >= carouselModalImages.length) {
+			carrouselCount = 0;
+		}
+		principalImage.src = carouselModalImages[carrouselCount].src;
+	});
+	previous.addEventListener("click", () => {
+		carrouselCount--;
+		if (carrouselCount <= 0) {
+			carrouselCount = carouselModalImages.length - 1;
+		}
+		principalImage.src = carouselModalImages[carrouselCount].src;
+	});
+
+	closeButton.addEventListener("click", () => {
+		isModalOpen = false;
+		images[0].classList.remove("selected");
+		images[1].classList.add("selected");
+		modal.remove();
+	});
+};
+
+principalImage.addEventListener("click", openModal);
